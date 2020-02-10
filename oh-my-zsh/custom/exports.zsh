5ea3d03563af4da75d651336c34592fb8a8647c7
Steve Grunwell <steve@stevegrunwell.com>
2020-02-10 12:56:52 -0500

    Use the Homebrew-installed version of Ruby instead of system Ruby

--- oh-my-zsh/custom/exports.zsh
+++ oh-my-zsh/custom/exports.zsh
@@ -16,3 +16,13 @@ export VVV_SKIP_LOGO=true
 
 # Enable developer mode for Slack
 export SLACK_DEVELOPER_MENU=true
+
+# Favor the Homebrew-installed version of Ruby
+export RUBY_HOME=/usr/local/opt/ruby/bin
+export GEM_PATH=~/.gem/ruby/2.6.0
+export PATH=$GEM_PATH/bin:$RUBY_HOME:$PATH
+
+# Load Node Version Manager (nvm)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
