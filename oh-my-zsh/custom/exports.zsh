ce4f8bfcb02fcc83056f7ea86d88836b4412f697
Steve Grunwell <steve@stevegrunwell.com>
2020-03-01 22:08:39 -0500

    Always use vim

--- oh-my-zsh/custom/exports.zsh
+++ oh-my-zsh/custom/exports.zsh
@@ -1,9 +1,5 @@
 # Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
-else
-  export EDITOR='mvim'
-fi
+export EDITOR='vim'
 
 # Don’t clear the screen after quitting a manual page
 export MANPAGER="less -X"
